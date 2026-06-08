6a6514955ff37dec74e6e13793d44a368dffaafd
Bektur_Akkaziev <bektur@MacBook-Pro-Bektur.local>
2026-06-08 14:23:45 +0600

    add Productadmin to admin.py

--- products/admin.py
+++ products/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 from .models import Category,Product
 
 admin.site.register(Category)
-admin.site.register(Product)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'category','price', 'remainder', 'created_at']
+    list_filter = ['category', 'name', 'created_at']
+    search_fields = ['name', 'category']
+    fields = [ 'name','description', 'category', 'price','remainder', 'created_at']
+    readonly_fields = ['created_at']
+
+admin.site.register(Product, ProductAdmin)
