0abba17287c228741b5796aa7beba15e0e81a731
Bektur_Akkaziev <bektur@MacBook-Pro-Bektur.local>
2026-06-08 17:49:38 +0600

    Modified model category and fixture

--- products/models.py
+++ products/models.py
@@ -1,10 +1,18 @@
 from django.db import models
+from django.utils.text import slugify
 
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True, verbose_name='Наименование')
     description = models.TextField(max_length=500, null=True, blank=True ,verbose_name='Описание')
+    slug = models.SlugField(max_length=100, unique=True, blank=True)
+
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
 
     class Meta:
         db_table = 'Категории'
