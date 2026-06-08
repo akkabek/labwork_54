d4d043ec4d37c9d22d3621b40a00bf7acb03e51b
Bektur_Akkaziev <bektur@MacBook-Pro-Bektur.local>
2026-06-08 14:42:21 +0600

    add productadmin class and update products_view.html

--- products/admin.py
+++ products/admin.py
@@ -7,7 +7,7 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'category','price', 'remainder', 'created_at']
     list_filter = ['category', 'name', 'created_at']
     search_fields = ['name', 'category']
-    fields = [ 'name','description', 'category', 'price','remainder', 'created_at']
+    fields = [ 'name','description', 'category', 'price','remainder', 'image']
     readonly_fields = ['created_at']
 
 admin.site.register(Product, ProductAdmin)
