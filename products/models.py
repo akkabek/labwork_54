dc5e4500b72d94e35bcb2be518c8a55ef64fd9d4
Bektur_Akkaziev <bektur@MacBook-Pro-Bektur.local>
2026-06-08 14:22:50 +0600

    add meta on category and make migrations

--- products/models.py
+++ products/models.py
@@ -6,15 +6,23 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        db_table = 'Категории'
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
 class Product(models.Model):
     name = models.CharField(max_length=100, verbose_name='Наименование')
     description = models.TextField(max_length=500, null=True, blank=True,verbose_name='Описание')
     category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата и время добавления')
-    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Стоимость')
+    price = models.DecimalField(max_digits=7, decimal_places=2, verbose_name='Стоимость')
     image = models.URLField()
+    remainder = models.PositiveIntegerField()
     def __str__(self):
         return self.name
 
     class Meta:
-        db_table = 'products'
+        db_table = 'Товары'
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
