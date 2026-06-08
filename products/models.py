881e25bbf7629e1592d0512b23e8c1ee1c9249e0
Bektur_Akkaziev <bektur@MacBook-Pro-Bektur.local>
2026-06-08 13:17:58 +0600

    Add verbose_name and class Meta

--- products/models.py
+++ products/models.py
@@ -1,19 +1,20 @@
 from django.db import models
 
-# Create your models here.
-
 class Category(models.Model):
-    name = models.CharField(max_length=100, unique=True)
-    description = models.TextField(max_length=500, null=True)
+    name = models.CharField(max_length=100, unique=True, verbose_name='Наименование')
+    description = models.TextField(max_length=500, null=True, blank=True ,verbose_name='Описание')
     def __str__(self):
         return self.name
 
 class Product(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField(max_length=500, null=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.TextField(max_length=500, null=True, blank=True,verbose_name='Описание')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата и время добавления')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Стоимость')
     image = models.URLField()
     def __str__(self):
         return self.name
+
+    class Meta:
+        db_table = 'products'
