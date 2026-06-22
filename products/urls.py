16e548ce2eb7af53d6bff286fe9109de322038d5
Bektur_Akkaziev <bektur@MacBook-Pro-Bektur.local>
2026-06-22 17:55:30 +0600

    Добавлена вьюшка и url для CartListView и CartAddView

--- products/urls.py
+++ products/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 from products.views import (ProductListView, ProductDetailView, ProductCreateView,
                             ProductUpdateView, ProductDeleteView)
+from products.views import CartListView, CartAddView
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='products'),
@@ -10,9 +11,13 @@ urlpatterns = [
     path('products/add/', ProductCreateView.as_view(), name='product_add'),
     path('products/<int:id>/edit/', ProductUpdateView.as_view(), name='product_edit'),
     path('products/<int:id>/delete/', ProductDeleteView.as_view(), name='product_delete'),
+
     path('categories/add/', views.category_add_view, name='category_add'),
     path('categories/', views.categories_view, name='categories_view'),
     path('categories/<int:id>/edit/', views.category_edit_view, name='category_edit'),
     path('categories/<int:id>/delete/', views.category_delete_view, name='category_delete'),
     path('products/<slug:slug>/', views.category_products_view, name='category_products'),
+
+    path('cart/', CartListView.as_view(), name='cart'),
+    path('cart/add/<int:pk>/', CartAddView.as_view(), name='cart_add'),
 ]
---
+++ products/views/cart.py
@@ -0,0 +1,36 @@
+from django.shortcuts import get_object_or_404, redirect
+from django.views import View
+from django.views.generic import ListView
+
+from products.models import Product, CartItem
+
+class CartListView(ListView):
+    template_name = 'cart/cart.html'
+    context_object_name = 'cart_items'
+
+    def get_queryset(self):
+        return CartItem.objects.select_related('product').all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        items = context['cart_items']
+        context['total'] = sum(item.total() for item in items)
+        return context
+
+class CartAddView(View):
+    def get(self, request, pk):
+        product = get_object_or_404(Product, pk=pk)
+
+        if product.remainder < 1:
+            return redirect('product_list')
+
+        cart_item = CartItem.objects.filter(product=product).first()
+
+        if cart_item is None:
+            CartItem.objects.create(product=product, quantity=1)
+        else:
+            if cart_item.quantity < product.remainder:
+                cart_item.quantity += 1
+                cart_item.save()
+
+        return redirect('product_list')
